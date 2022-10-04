4ef8be32c0aac8ffff4da20db662e024e47b0cc8
Gustavo Santamaria Velazquez <gusstamria@hotmail.com>
2022-10-03 20:55:04 -0500

    ya casi

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -162,7 +162,7 @@ function redondearNumero(num) {
 function redondearHaciaArriba(num) {
   // Redondea "num" hacia arriba (al próximo entero) y devuélvelo
   // Tu código:
-  return (Math.floor(num));
+  return (Math.ceil(num));
 }
 
 function numeroRandom() {
@@ -177,32 +177,45 @@ function esPositivo(numero) {
   //Si el número es positivo, devolver ---> "Es positivo"
   //Si el número es negativo, devolver ---> "Es negativo"
   //Si el número es 0, devuelve false
+  if (numero>0){
+    return ("Es positivo");
+  }
+  if(numero<0){
+    return ("Es negatico");
+  }
   
+    return (false);
+    
 }
 
 function agregarSimboloExclamacion(str) {
   // Agrega un símbolo de exclamación al final de la string "str" y devuelve una nueva string
   // Ejemplo: "hello world" pasaría a ser "hello world!"
   // Tu código:
+  return (str+'!');
 }
 
-function combinarNombres(nombre, apellido) {
+
+
+function combinarNombres(nombre,apellido) {
   // Devuelve "nombre" y "apellido" combinados en una string y separados por un espacio.
   // Ejemplo: "Soy", "Henry" -> "Soy Henry"
   // Tu código:
-  
+  return (nombre+'  '+apellido);
 }
 
 function obtenerSaludo(nombre) {
   // Toma la string "nombre" y concatena otras string en la cadena para que tome la siguiente forma:
   // "Martin" -> "Hola Martin!"
   // Tu código:
+  return ('hola'+'   '+nombre);
   
 }
 
 function obtenerAreaRectangulo(alto, ancho) {
   // Retornar el area de un rectángulo teniendo su altura y ancho
   // Tu código:
+  return ((alto*ancho)/2);
   
 }
 
@@ -210,6 +223,7 @@ function obtenerAreaRectangulo(alto, ancho) {
 function retornarPerimetro(lado){
   //Escibe una función a la cual reciba el valor del lado de un cuadrado y retorne su perímetro.
   //Escribe tu código aquí
+  return (lado*4);
   
 }
 
